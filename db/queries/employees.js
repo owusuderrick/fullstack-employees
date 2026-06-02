2d6bf862bd207d50abc09840d8f0044b33edd926
Derrick Owusu <owusuderek@gmail.com>
2026-06-02 12:27:26 -0700

    Completed

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,35 +1,48 @@
 /** @returns the employee created according to the provided details */
 export async function createEmployee({ name, birthday, salary }) {
   // TODO
-}
+  const client = require("../client");
 
-// === Part 2 ===
+  async function createEmployee({ name, birthday, salary }) {
+    const sql = `
+      INSERT INTO employees (name, birthday, salary)
+      VALUES ($1, $2, $3)
+      RETURNING *
+    `;
+    const {
+      rows: [employee],
+    } = await db.query(sql, [name, birthday, salary]);
+    return employee;
+  }
 
-/** @returns all employees */
-export async function getEmployees() {
-  // TODO
-}
+  // === Part 2 ===
 
-/**
- * @returns the employee with the given id
- * @returns undefined if employee with the given id does not exist
- */
-export async function getEmployee(id) {
-  // TODO
-}
+  /** @returns all employees */
+  export async function getEmployees() {
+    // TODO
+  }
 
-/**
- * @returns the updated employee with the given id
- * @returns undefined if employee with the given id does not exist
- */
-export async function updateEmployee({ id, name, birthday, salary }) {
-  // TODO
-}
+  /**
+   * @returns the employee with the given id
+   * @returns undefined if employee with the given id does not exist
+   */
+  export async function getEmployee(id) {
+    // TODO
+  }
 
-/**
- * @returns the deleted employee with the given id
- * @returns undefined if employee with the given id does not exist
- */
-export async function deleteEmployee(id) {
-  // TODO
+  /**
+   * @returns the updated employee with the given id
+   * @returns undefined if employee with the given id does not exist
+   */
+  export async function updateEmployee({ id, name, birthday, salary }) {
+    // TODO
+  }
+
+  /**
+   * @returns the deleted employee with the given id
+   * @returns undefined if employee with the given id does not exist
+   */
+  export async function deleteEmployee(id) {
+    // TODO
+  }
 }
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { createEmployee } from "./queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -7,4 +8,21 @@ console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
   // TODO
+
+  const employees = [
+    { name: "Paula Nielson", birthday: "1985-03-12", salary: 84000 },
+    { name: "Robert Smith", birthday: "1990-07-22", salary: 920000 },
+    { name: "Stacy Uy", birthday: "1988-11-05", salary: 85000 },
+    { name: "Gregory Umukoro", birthday: "1992-01-17", salary: 90000 },
+    { name: "Kim Young", birthday: "1983-09-30", salary: 89000 },
+    { name: "Jose Acosta", birthday: "1995-04-14", salary: 75000 },
+    { name: "David Sun", birthday: "1987-06-09", salary: 79000 },
+    { name: "Curtis Jones", birthday: "1991-12-02", salary: 69000 },
+    { name: "Prince Khalifa", birthday: "1984-08-19", salary: 82000 },
+    { name: "Judith Day", birthday: "1993-10-27", salary: 76000 },
+  ];
+
+  for (const emp of employees) {
+    await createEmployee(emp);
+  }
 }
